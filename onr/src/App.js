71ffb08d4e39778c512dcb67ccac6e2f2eb528f1
hannah huang <hannahhuang000@outlook.com>
2021-02-17 18:58:13 -0500

    done css

--- onr/src/App.js
+++ onr/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import React from 'react';
-import {Grid, Typography, Button, Box, Card, CardContent, } from '@material-ui/core';
+import {Grid, Typography, Button, Box, Card, CardContent, GridList,GridListTile,GridListTileBar} from '@material-ui/core';
 import LanguageIcon from '@material-ui/icons/Language';
 import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import styles from './App.module.css';
-
+import InfoIcon from '@material-ui/icons/Info';
 import PostData from './PostData';
 
 
@@ -21,12 +21,12 @@ function App() {
 
         
           <Grid item xs={12} sm={4} className={styles.smallcontainer}>Filters
-          <Grid item xs={12} sm={6} className = {styles.table1}>
-            <Box border={1} borderRadius={10} boxShadow={3}>
-            <Grid item sm = {6} ><CheckBoxIcon style={{ fill: '#03a9f4' }}/> All CQA Results</Grid>
-            <Grid item sm = {6}><CheckBoxIcon style={{ fill: '#e1f5fe' }}/>CQAs with Closed Loop</Grid>
+          
+            <Box border={1} borderRadius={10} boxShadow={3} className={styles.table1}>
+            <Grid item sm = {6} ><CheckBoxIcon style={{ fill: '#03a9f4' }}/> All CQA Results<InfoIcon /></Grid>
+            <Grid item sm = {6}><CheckBoxIcon style={{ fill: '#e1f5fe' }}/>CQAs with Closed Loop<InfoIcon /></Grid>
             </Box>
-          </Grid>
+          
           </Grid>
 
           <Grid item xs={12} sm={4} className={styles.bigcontainer}>QUALITY SCORE TREND</Grid>
@@ -42,108 +42,10 @@ function App() {
 
       <Grid container className={styles.container}>
         <Grid item xs={3} className={styles.gauge}>
-
-          <Grid container spacing={24}>
-            <Grid item  xs={4} className={styles.card}>
-            <Card>
-            <CardContent>
-                <Typography color='primary' variant='h5' component='h4'gutterBottom>
-                    Quality Score
-                </Typography>
-                <Typography color='primary'   component='p'>
-                  picture Here
-                </Typography>
-                <Typography color='primary'   component='p'>
-                  Sample: 133
-                </Typography>
-            </CardContent>
-            </Card>
-            </Grid> 
-            <Grid item  xs={4} className={styles.card}>
-            <Card>
-            <CardContent>
-                <Typography color='primary' variant='h5' component='h4'gutterBottom>
-                    Quality Score
-                </Typography>
-                <Typography color='primary'   component='p'>
-                  picture Here
-                </Typography>
-                <Typography color='primary'   component='p'>
-                  Sample: 133
-                </Typography>
-            </CardContent>
-            </Card>
-            </Grid> 
-            <Grid item  xs={4} className={styles.card}>
-            <Card>
-            <CardContent>
-                <Typography color='primary' variant='h5' component='h4'gutterBottom>
-                    Quality Score
-                </Typography>
-                <Typography color='primary'   component='p'>
-                  picture Here
-                </Typography>
-                <Typography color='primary'   component='p'>
-                  Sample: 133
-                </Typography>
-            </CardContent>
-            </Card>
-            </Grid> 
-          </Grid>
-
-          <Grid container spacing={24}>
-            <Grid item  xs={4} className={styles.card}>
-            <Card>
-            <CardContent>
-                <Typography color='primary' variant='h5' component='h4'gutterBottom>
-                    Quality Score
-                </Typography>
-                <Typography color='primary'   component='p'>
-                  picture Here
-                </Typography>
-                <Typography color='primary'   component='p'>
-                  Sample: 133
-                </Typography>
-            </CardContent>
-            </Card>
-            </Grid> 
-            <Grid item  xs={4} className={styles.card}>
-            <Card>
-            <CardContent>
-                <Typography color='primary' variant='h5' component='h4'gutterBottom>
-                    Quality Score
-                </Typography>
-                <Typography color='primary'   component='p'>
-                  picture Here
-                </Typography>
-                <Typography color='primary'   component='p'>
-                  Sample: 133
-                </Typography>
-            </CardContent>
-            </Card>
-            </Grid> 
-            <Grid item  xs={4} className={styles.card}>
-            <Card>
-            <CardContent>
-                <Typography color='primary' variant='h5' component='h4'gutterBottom>
-                    Quality Score
-                </Typography>
-                <Typography color='primary'   component='p'>
-                  picture Here
-                </Typography>
-                <Typography color='primary'   component='p'>
-                  Sample: 133
-                </Typography>
-            </CardContent>
-            </Card>
-            </Grid> 
-          </Grid>
-
-
-
-
-
-          
+        {/* <Grid spacing={3} justify='center'>
+          <PostData />
+        </Grid> */}
+         <PostData />
         </Grid>
 
 
--- onr/src/PostData.js
+++ onr/src/PostData.js
@@ -1,17 +1,61 @@
 import React,{createRef, Component} from 'react';
 import Data from './data.json';
+import {Grid, Typography, Button, Box, Card, CardContent, GridList,GridListTile,GridListTileBar} from '@material-ui/core';
+import styles from './PostData.module.css';
 
 class PostData extends Component {
     constructor(props) {
         super(props);
         this.ref = createRef();
-
+        this.state = {
+            data:{
+                gaugeData:Data.gaugeData
+            }
+        };
     }
     render(){
+        console.log('PostData is: ',this.state.data.gaugeData);
         return (
-        <div>wow</div>
+        <Grid container spacing ={1}>
+        {this.state.data.gaugeData.map(function(item, index) {
+            return <Grid  item xs={4} spacing={3}>
+                <Card>
+                <CardContent>
+                    <Typography color='primary' variant='h6' component='h6' gutterBottom>
+                        {item.name}
+                    </Typography>
+                    <Typography color='primary'   component='p'>
+                    picture Here
+                    </Typography>
+                    <Typography color='primary'   component='p'>
+                    Sample: {item.sample}
+                    </Typography>
+                </CardContent>
+                </Card>
+            </Grid>
+            
+        })
+        }
+        </Grid>
+        
         )
     }
 }
 
 export default PostData;
+
+{/* <Grid item  xs={4} className={styles.card} key={index}>
+                <Card>
+                <CardContent>
+                    <Typography color='primary' variant='h5' component='h4'gutterBottom>
+                        {item.name}
+                    </Typography>
+                    <Typography color='primary'   component='p'>
+                    picture Here
+                    </Typography>
+                    <Typography color='primary'   component='p'>
+                    Sample: {item.sample}
+                    </Typography>
+                </CardContent>
+                </Card>
+                </Grid> */}
